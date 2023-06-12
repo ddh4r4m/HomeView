88a416841251c84263d7dd428380c31bfb3131d9
Dharam Dhurandhar <dharamdhurandhar@Dharams-MacBook-Pro.local>
2023-06-12 20:50:29 +0530

    Initial Commit

---
+++ HomeView/HomeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewApp.swift
+//  HomeView
+//
+//  Created by Dharam Dhurandhar on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
